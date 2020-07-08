40a4368599503b806394348cff3ba8afb0984c8d
mildchae <chaeefe@gmail.com>
2020-07-08 14:26:12 +0900

    chore: chore

--- srceens/Detail.js
+++ srceens/Detail.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
-import { Button, StyleSheet, View, Text, TextInput, Dimensions, Modal, TouchableOpacity } from 'react-native'
-import Icon from 'react-native-vector-icons/FontAwesome'
+import { Button, StyleSheet, View, Text, TextInput, Dimensions, TouchableOpacity } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import AddButton from '../components/add-button'
-
+import StockModal from '../components/StockModal'
 function DetailsScreen({ navigation }) {
     const [name, onChangeName] = useState('')
     const [volumn, onChangeVolumn] = useState('')
     const [modal, setModal] = useState(false)
+
     return (
       <View style={styles.container}>
         <View style={styles.initSetting}>
@@ -24,43 +25,21 @@ function DetailsScreen({ navigation }) {
           />
           <View style = {styles.lineStyle} />
           
-          <AddButton title='add stock' onPress={() => {setModal(true)}} />
+          {/* <AddButton title='add stock' onPress={() => {setModal(true)}} /> */}
+          <AddButton title='add stock' onPress={() => navigation.navigate('pick')} />
+
         </View>
 
+        {/* <StockModal modal = {modal} setModal ={setModal}/> */}
         <View style={styles.HeaderContainer}>
           <TouchableOpacity style={styles.backicon} onPress={() => navigation.goBack()}>
-            <Icon name="arrow-left" size={25} color="#fff"  />         
+            <Icon name="keyboard-arrow-left" size={35} color="#fff"  />         
           </TouchableOpacity>
     
           <Text style={styles.headerText}>
             포트폴리오 만들기
           </Text>
         </View>
-
-        <Modal visible={ modal }
-        transparent={true}
-        onRequestClose={_ => setModal(false) }>
-            <TouchableOpacity style={styles.modalBackground} onPress={_ => setModal(false)}>
-
-            </TouchableOpacity>
-            <View style={styles.modalContainer}>
-                <View style={styles.modalHeader}>
-                  <Text style={{flex:1, textAlign:'center', fontSize:20}}>add stock</Text>
-                  <TouchableOpacity style={styles.closeicon} onPress={_ => setModal(false) }>
-                      <Icon name="close" size={30} color="#333" />
-                  </TouchableOpacity>
-                </View>
-                <View style={styles.modalContent}>
-                  <TextInput
-                    style={styles.textInput}
-                    onChangeText={text => onChangeVolumn(text)}
-                    value={volumn}
-                    placeholder="total volumn($)"
-                  />
-                </View>
-
-            </View>
-        </Modal>
 
       </View>
     );
@@ -92,14 +71,14 @@ function DetailsScreen({ navigation }) {
         top: (Platform.OS == 'ios') ? 20 : 0,
         left: 0,
         right: 0,
-        height: 70,
+        height: 60,
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
         borderBottomLeftRadius: 8,
         borderBottomRightRadius: 8,
         // paddingTop: Constants.statusBarHeight,
-        backgroundColor: 'rgb(106,188,86)',
+        backgroundColor: 'rgb(133,162,228)',
         
       },
       backicon: {
@@ -121,38 +100,7 @@ function DetailsScreen({ navigation }) {
         width: Dimensions.get('window').width * 0.8
 
       },
-      modalContainer: {
-       backgroundColor : 'white',
-       position: 'absolute',
-       bottom: 0,
-       width: '100%', 
-       height: '40%',
-       borderTopRightRadius: 16,
-       borderTopLeftRadius: 16,
-       zIndex: 2,
 
-      },
-      modalBackground: {
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'rgba(0,0,0,0.6)',
-        zIndex: 1
-      },
-      modalHeader:{
-        // backgroundColor: 'red',
-        flexDirection: 'row',
-        marginTop: 20,
-        alignItems: 'center'
-      },
-      closeicon:{
-        position: 'absolute',
-        right: 20,
-
-      },
-      modalContent:{
-        justifyContent: 'center',
-        alignItems: 'center',
-      }
 
     }
   )
